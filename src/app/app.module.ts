31686866d21c611aca095b52357f42b3a438adcf
unknown <you@example.com>
2024-02-21 11:57:48 +0530

    chnanges

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { NgSelectModule } from '@ng-select/ng-select';
+
 
 
 
@@ -15,7 +15,7 @@ import { NgSelectModule } from '@ng-select/ng-select';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    NgSelectModule
+    
 
  ],
   providers: [],
